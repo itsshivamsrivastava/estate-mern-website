e24cb7a3da92ecb99f5934654d5d03caed589f52
Shivam Srivastava <shivamsri142@gmail.com>
2025-02-27 16:15:58 +0530

    Create pages and routes

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+//Tailwind CSS Docs: https://v3.tailwindcss.com/docs/guides/vite
+// https://www.youtube.com/watch?v=VAaUy_Moivw
+// continue from 23:35
